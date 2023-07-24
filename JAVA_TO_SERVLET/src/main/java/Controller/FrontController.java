4c8f4e5b58487e108c65accf5bd68827ebcd1949
kimgyeongmin <orange761943@naver.com>
2023-07-24 09:27:33 +0900

    0724_2

---
+++ JAVA_TO_SERVLET/src/main/java/Controller/FrontController.java
@@ -0,0 +1,40 @@
+package Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrontController {
+
+	private Map<String,SubController> map = new HashMap();
+	//초기값 설정
+	//사용자요구사항API Controller에 맞게 저장
+	// /req_bookinfo에대한 요구사항은 BookController 전달
+	
+	private void init() {
+		//도서정보 요구사항-Controller Mapping()
+		// /RequestUri ServiceNo Param
+		map.put("/book", new BookController());		
+		map.put("/member", new MemberController());
+		map.put("/lend", new LendController());
+		
+	}
+	
+	public FrontController(){
+		init();
+	}
+	
+	//request에 맞는 Controller를 추출,해당컨트롤러 실행
+	//request , ServiceNo(1 select,2 insert , 3 update , 4 delete) , param
+	public Map<String,Object> execute(String request,int ServiceNo ,Map<String,Object> param) {
+		  SubController controller =map.get(request);
+		  
+		  Map<String,Object> result = new HashMap();
+		  result = controller.execute(ServiceNo, param);
+		  
+		  
+		  return result;
+		  
+	}
+	
+	
+}
